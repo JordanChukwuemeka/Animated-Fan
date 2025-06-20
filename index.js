3eea3f26bfa74fd975c21a9d32510ec33dd78c4a
Jordan Chukwuemeka <108676737+JordanChukwuemeka@users.noreply.github.com>
2025-06-20 20:37:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var img = document.querySelector("img");
+
+function myfanOn () {
+    img.style.animationDuration = "2s";
+}
+
+function myfanOff() {
+    img.style.animationDuration = "0s";
+}
+
+function myfan1() {
+    img.style.animationDuration = "1s";
+}
+
+function myfan2() {
+    img.style.animationDuration = "0.5s";
+}
+
+function myfan3() {
+    img.style.animationDuration = "0.1s";
+}
